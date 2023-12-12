4c644ba6e077318deca1f309180507326c6d8864
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-12-13 01:34:02 +0200

    added the ride_request component

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter,Routes,Route} from 'react-router-dom';
+import RideRequest from './components/RideRequest';
+
 function App() {
   return (
     <div className="app">
