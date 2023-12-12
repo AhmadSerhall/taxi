9d34750563c65142ce29947978829f41609f68fd
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-12 23:01:02 +0200

    admin login

---
+++ electron/src/components/login/index.js
@@ -0,0 +1,74 @@
+import React, { useRef } from "react";
+import { sendRequest } from "../../core/request";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+
+  const saveToken = async () => {
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+
+    try {
+      const data = await sendRequest("login", "POST", formData);
+      console.log("data", data.authorisation.token);
+
+      if (data.user) {
+        localStorage.setItem("token", data.authorisation.token);
+        navigate("/admin_panel");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="lgn-container">
+      <div className="login-container">
+        <h1 className="login-title">Login</h1>
+        <div className="login-form">
+          <label htmlFor="email" className="username_title">
+            Email
+            <br />
+            <br />
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="Enter your email"
+            ref={emailRef}
+          />
+          <label htmlFor="password" className="password_title">
+            Email
+            <br />
+            <br />
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            ref={passwordRef}
+          />
+          <div className="login-button-div">
+            <button
+              type="submit"
+              className="login-button"
+              id="login-button"
+              onClick={saveToken}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
