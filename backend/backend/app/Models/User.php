1581566c48e198de1dcf0b9a0dc49f4842bc5fc9
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-12 05:35:32 +0200

    add user controller + register for driver

--- backend/backend/app/Models/User.php
+++ backend/backend/app/Models/User.php
@@ -20,7 +20,6 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'first_name',
         'last_name',
-        'image',
         'email',
         'password',
         'role_id',
--- backend/backend/routes/api.php
+++ backend/backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'login');
@@ -11,3 +12,13 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('refresh', 'refresh');
 
 });
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('get_users', 'get_users');
+    Route::post('add_user', 'add_user');
+    Route::post('update_user', 'update_user');
+
+});
+
+
+Route::delete('delete_user/{id}', [UserController::class, 'delete_user']);
