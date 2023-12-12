71c2c66d0bce1e3a79d7d963690c4d193c018685
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-12 22:45:42 +0200

    finished the dropdown menus

---
+++ frontend/frontend/src/components/drop_down_type/index.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from 'react';
+import './style.css';
+import '../../styles/global.css';
+
+const Dropdown2 = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className="dropdown" ref={dropdownRef}>
+      <div className={`dropdown-header prim-clr-bg ${isOpen ? 'open' : ''}`} onClick={toggleDropdown}>
+        {selectedOption ? selectedOption : 'Select your type as user'}
+        <i className={`fas fa-chevron-down icon ${isOpen ? 'open' : ''}`} />
+      </div>
+      {isOpen && (
+        <ul className="dropdown-options">
+          {options.map((option) => (
+            <li key={option} onClick={() => handleOptionClick(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown2;
