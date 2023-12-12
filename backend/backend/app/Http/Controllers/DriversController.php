a926020095364c07c4d9dcd107f1b2cfc68bebf3
Zein Shhadi <zeinshhadi@hotmail.com>
2023-12-12 05:43:02 +0200

    fixed driver controller

--- backend/backend/app/Http/Controllers/DriversController.php
+++ backend/backend/app/Http/Controllers/DriversController.php
@@ -8,24 +8,8 @@ use Illuminate\Support\Facades\Auth;
 
 class DriversController extends Controller
 {
-  public function add_driver(Request $req){
 
-        // $driver = Driver::create([
-        // 'first_name'=>$req->first_name,
-        // 'last_name'=>$req->last_name,
-        // 'image'=>$req->image,
-        // 'email'=>$req->email,
-        // 'password'=>$req->password,
-        // 'role_id'=>$req->role_id,
-        // 'gender'=>$req->gender,
-        // 'status'=>$req->status,
-        // 'car'=>$req->car,
-        // 'vin'=>$req->vin,
-            
-        // ]);
-$driver = Driver::create($req->all());
 
-  }
 
     public function delete_driver(Request $req){
     if(Auth::check()){
--- backend/backend/app/Models/Driver.php
+++ backend/backend/app/Models/Driver.php
@@ -9,17 +9,6 @@ class Driver extends Model
 {
     use HasFactory;
     protected $primaryKey = 'driver_id';
-    protected $fillable = [
-        'first_name',
-        'last_name',
-        'image',
-        'email',
-        'password',
-        'role_id',
-        'gender',
-        'status',
-        'car',
-        'vin',
-        'user_id'];
+    protected $fillable = ['status'];
 
 }
