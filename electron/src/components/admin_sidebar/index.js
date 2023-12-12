74a3cd8e8d9b4ff0c6c000e3dc077ee3610d66dc
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-12-12 22:28:31 +0200

    admin side bar component

---
+++ electron/src/components/admin_sidebar/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const AdminSidebar = (props) => {
+  const navigate = useNavigate();
+
+  const goDashboards = () => {
+    navigate("/admin_panel");
+  };
+  const goPassengers = () => {
+    navigate("/admin_panel/passengers");
+  };
+  const goDrivers = () => {
+    navigate("/admin_panel/drivers");
+  };
+  const goRides = () => {
+    navigate("/admin_panel/rides");
+  };
+
+  return (
+    <div className="side-panel">
+      <div className="dashboard-btn" id="dashboards-btn" onClick={goDashboards}>
+        Dashboard
+      </div>
+      <div className="passenger-btn" id="passenger-btn" onClick={goPassengers}>
+        Passengers
+      </div>
+      <div className="driver-btn" id="driver-btn" onClick={goDrivers}>
+        Drivers
+      </div>
+      <div className="ride-btn" id="ride-btn" onClick={goRides}>
+        Rides
+      </div>
+    </div>
+  );
+};
+
+export default AdminSidebar;
