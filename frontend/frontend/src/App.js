14893ff687377e5b5aa8575885016e63f2463579
AhmadShamouka <shamoukaahmad@gmail.com>
2023-12-13 13:03:51 +0200

    fixing CSS styling

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
+import Login from "./pages/login";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/navbar" element={<Navbar />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/frontend/src/index.js
+++ frontend/frontend/src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import RideRequest from './components/ride_request';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import RideRequest from "./components/ride_request";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RideRequest passenger_location = {'location 1'} destination = {'location 2'} driver_name = {'driver name'} request_date = {'02/08/2023'} status = {'Completed'} rate = {3}/>
-    <RideRequest passenger_location = {'location 1'} destination = {'location 2'} driver_name = {'driver name'} request_date = {'02/08/2023'} status = {'Abandoned'} rate = {3}/>
-    <RideRequest passenger_location = {'location 1'} destination = {'location 2'} driver_name = {'driver name'} request_date = {'02/08/2023'} status = {'Pending'} rate = {3}/>
+    <App />
   </React.StrictMode>
 );
-
