923c78893c47718cf36b2d807798a2c65823b4a6
AhmadShamouka <shamoukaahmad@gmail.com>
2023-12-13 02:15:41 +0200

    navbar styling

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
-import SignUp from './pages/signup';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/signup";
+import Navbar from "./components/navbar/index";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-      <Route path='/signup' element={<SignUp/>} />
-      </Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<Navbar />} />
+        </Routes>
       </BrowserRouter>
-      </div>
+    </div>
   );
 }
 
--- frontend/frontend/src/components/navbar/index.jsx
+++ frontend/frontend/src/components/navbar/index.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import "./index.css";
-function index(props) {
+import logo from "../../assets/logo.png";
+import pp from "../../assets/pp.png";
+import "./style.css";
+const Navbar = () => {
   return (
     <section className="container">
-      <div className="logo"></div>
+      <div className="logo">
+        <img src={logo} alt="null" />
+      </div>
       <div className="title">
         <h1>Search Riders</h1>
       </div>
       <div className="button-container">
-        <button className="btn">Home</button>
-        <button className="btn">Services</button>
-        <button className="btn">Contact Us</button>
+        <a className="btn" onClick={() => this.handleClick("Contact Us")}>
+          Home
+        </a>
+        <a className="btn" onClick={() => this.handleClick("Contact Us")}>
+          Services
+        </a>
+        <a className="btn" onClick={() => this.handleClick("Contact Us")}>
+          Contact Us
+        </a>
       </div>
-      <div className="profile"></div>
+      <div className="profile">
+        <img src={pp} alt="null" />
+      </div>
     </section>
   );
-}
-
-export default index;
+};
+export default Navbar;
