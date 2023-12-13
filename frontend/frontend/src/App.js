c13a33d6ec7042df1040eb6a5426581691139d13
AhmadShamouka <shamoukaahmad@gmail.com>
2023-12-13 22:56:23 +0200

    fixing bugs

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
+import Login from "./pages/login";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
