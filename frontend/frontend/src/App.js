8d8a88c7519206f5be603241afa5075bc75ad0e8
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-12-13 21:15:13 +0200

    continuing

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
-import HomePage from './pages/landing';
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/HomePage" element = {<HomePage/>} /> 
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/frontend/src/pages/landing/index.jsx
+++ frontend/frontend/src/pages/landing/index.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import Button from "../../components/button"
-import TitleLogo from '../../components/title_logo'
+import logo from "../../assets/logo.png";
+import pp from "../../assets/pp.png";
+import "./style.css";
+import home_background from "../../assets/home_background.png"
 
 const HomePage = () => {
-    <div className='homepage'>
-        <TitleLogo/>
-    </div>
+    return(
+        <div className='homepage'>
+            <div className='home_img'>
+                <img src= {home_background} alt="home_background" />
+            </div>
+            <div className='home_container'>
+
+                <div className='home_nav'>
+                    <div className='nav_left'>
+                        <div className='logo_img'>
+                            <img src={logo} alt="logo" />
+                        </div>
+                        <ul >
+                            <li>Home</li>
+                            <li>Services</li>
+                            <li>About Us</li>
+                        </ul>
+                    </div>
+                    <div className='profile_img'>
+                        <img src={pp} alt="pp" />
+                    </div>
+                </div>
+
+                <div className='home_content'>
+
+                    <div>
+                        <p>Discover Seamless Transportation with Relax Taxi.</p>
+                    </div>
+
+                    <button onClick={console.log('Browse')}>Browse Our Services</button>
+                </div>
+            </div>
+        </div>
+    )
+
 }
 
 export default HomePage
