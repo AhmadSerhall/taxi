0240c70c4830b0c3b32d8ca0ce8ce30bcc68cb8d
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-13 21:08:53 +0200

    pushing rating and rides pages

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -2,13 +2,18 @@ import "./App.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
 import Login from "./pages/login";
+import Rating from "./pages/rating";
+import Map from "./pages/rides";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route classname="signup"path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login/>} />    
+          <Route path="/login" element={<Login/>} />
+          <Route path="/rating" element={<Rating/>} />  
+          <Route path="/ride" element={<Map/>} />
+          
         </Routes>
       </BrowserRouter>
     </div>
