56779add91d7fd3ebc9cb7ce74608c370b8d5784
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-13 10:56:38 +0200

    commiting

--- backend/backend/app/Http/Controllers/RideController.php
+++ backend/backend/app/Http/Controllers/RideController.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class RiderController extends Controller
+class RideController extends Controller
 {
 public function approve_ride(Request $req){
     if(Auth::check()){
