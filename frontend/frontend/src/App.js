c87886dc107ff71aaea47a28122c803e1895cf6c
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-12-13 19:07:23 +0200

    merged from the develop to this branch

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,17 +1,14 @@
-<<<<<<< HEAD
 import React from 'react';
-import { BrowserRouter,Routes,Route} from 'react-router-dom';
-=======
->>>>>>> develop
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
+import HomePage from './pages/landing';
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/HomePage" element = {<HomePage/>} /> 
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/frontend/src/pages/landing/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from "../../components/button"
+import TitleLogo from '../../components/title_logo'
+
+const HomePage = () => {
+    <div className='homepage'>
+        <TitleLogo/>
+    </div>
+}
+
+export default HomePage
