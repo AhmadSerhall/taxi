701ae037f1187c703dbc46387519615b1da1ff9f
Zein Shhadi <zeinshhadi@hotmail.com>
2023-12-13 20:33:48 +0200

    history requests for driver

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signup";
 import YourRides from "./pages/yourrides";
+import DriversRequest from "./pages/driver_ride_requests";
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/yourrides" element={<YourRides />} />
+          <Route path="/yourrides" element={<DriversRequest />} />
         </Routes>
       </BrowserRouter>
     </div>
