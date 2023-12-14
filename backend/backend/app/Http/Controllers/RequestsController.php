553879b71b4fcc9225d5d868d73192b7f7c9deb7
Zein Shhadi <zeinshhadi@hotmail.com>
2023-12-14 07:07:44 +0200

    showing pending requests

--- backend/backend/app/Http/Controllers/RequestsController.php
+++ backend/backend/app/Http/Controllers/RequestsController.php
@@ -16,10 +16,22 @@ class RequestsController extends Controller
         if (Auth::Check()){
              
             $user = Auth::user();
-         
+    
             if($user && $user->role_id == 3){
  
                 $request = Crequest::create(['user_id' => $user->user_id, 'driver_id'=>$req->driver_id]);
+                 $request = Crequest::where('driver_id', $user->driver_id)->update(['status' => 'pending']);
+                     $ride=Ride::create([
+                        'driver_id'=> $req->driver_id,
+                        'user_id'=>$user->user_id,
+                        'location_start'=>30,
+                        'location_end'=>30,
+                        'status'=>'pending',
+                        'price'=>20,
+                        'rate_user'=>0,
+                        'rate_driver'=>0,
+
+                    ]); 
                 return response()->json([
                     'status' => 'success',]);
             }else{
--- frontend/frontend/src/components/driver_requests/index.jsx
+++ frontend/frontend/src/components/driver_requests/index.jsx
@@ -15,30 +15,35 @@ const DriverRequest = ({ passenger_location, destination, driver_name, request_d
   }, []);
   return (
     <div>
-      <div className="container flex space-around ">
-        <div className="left flex column">
-          <h1>Latest Request</h1>
-          <div className="request_card_l flex column space-around">
-            <p className="txt">FROM: {passenger_location}</p>
-
-            <p className="txt">To: {destination}</p>
-
-            <p className="txt">Driver Name: {driver_name}</p>
-          </div>
-        </div>
-        <div className="request_card_r flex column space-around">
-          <p className="txt">Request Date: {request_date}</p>
-          <div className="flex center">
-            <p className="txt">Status:</p>
-            <div className="flex">
-              <button className="btn-app"> Approve </button>
-              <button className="btn-rej"> rejected </button>
+      {rides.length > 0 ? (
+        rides.map((ride, index) =>
+          ride.status === "pending" ? (
+            <div key={index} className="container flex space-around">
+              <div className="left flex column">
+                <h1>Latest Request</h1>
+                <div className="request_card_l flex column space-around">
+                  <p className="txt">FROM: {ride.location_start}</p>
+                  <p className="txt">To: {ride.location_end}</p>
+                  <p className="txt">Driver Name: {ride.driver_id}</p>
+                </div>
+              </div>
+              <div className="request_card_r flex column space-around">
+                <p className="txt">Request Date: {ride.created_at}</p>
+                <div className="flex center">
+                  <p className="txt">Status:</p>
+                  <div className="flex">
+                    <button className="btn-app"> Approve </button>
+                    <button className="btn-rej"> Reject </button>
+                  </div>
+                </div>
+                <p className="txt">Cost: {ride.price}</p>
+              </div>
             </div>
-          </div>
-
-          <p className="txt">Cost: 20</p>
-        </div>
-      </div>
+          ) : null
+        )
+      ) : (
+        <p>No history</p>
+      )}
     </div>
   );
 };
