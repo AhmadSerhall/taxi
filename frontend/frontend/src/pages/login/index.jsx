78378280144e64da3f173f68cb306f7d76debef6
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-12-14 10:49:53 +0200

    old changes

--- frontend/frontend/src/pages/login/index.jsx
+++ frontend/frontend/src/pages/login/index.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import "../../styles/global.css";
 import TitleLogo from "../../components/title_logo";
 import Input from "../../components/input";
 import Button from "../../components/button";
+import axios from "axios";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/getchat", {
+        email,
+        password,
+      });
+      console.log("Login successful:", response.data);
+    } catch (error) {
+      console.error("Login failed:", error.message);
+    }
+  };
   return (
     <div className="center">
       <TitleLogo />
@@ -14,20 +30,24 @@ const Login = () => {
           <Input
             className="input"
             name="email"
-            placeholder="enter your email"
-            label="email"
+            placeholder="Enter your email"
+            label="Email"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             className="input"
             name="password"
-            placeholder="enter your password"
-            label="password"
+            type="password"
+            placeholder="Enter your password"
+            label="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Button
             className="button-login"
             color="black"
             backgroundColor="#D9D9D8"
-            text="login"
+            text="Login"
+            onClick={handleLogin}
           />
         </div>
       </div>
