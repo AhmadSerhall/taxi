78bf2cb1ade6e01f386a6ef7a9682de261b2505b
Zein Shhadi <zeinshhadi@hotmail.com>
2023-12-14 06:43:56 +0200

    fetched user data according to status

--- frontend/frontend/src/components/rides_history/index.jsx
+++ frontend/frontend/src/components/rides_history/index.jsx
@@ -8,6 +8,7 @@ const RidesHistory = () => {
       .get("http://localhost:8000/api/rides")
       .then(function (res) {
         setRide(res.data);
+        console.log(rides);
       })
       .catch(function (error) {
         console.log("error");
@@ -17,30 +18,31 @@ const RidesHistory = () => {
     <div>
       <div className="container-rides flex column">
         <h1>Latest Rides</h1>
-        {rides.map((ride, index) => (
-          <div className="ride-history-details flex space-between">
-            <div className="ride-history-details-left flex column space-around">
-              <p key={index} className="txt">
-                FROM: {ride.location_start}
-              </p>
-
-              <p className="txt">To: {ride.location_end}</p>
-
-              <p className="txt">Driver Name: {ride.driver_id}</p>
-            </div>
-            <div className="ride-history-details-right flex column space-around">
-              <p className="txt">Request Date: {ride.created_at}</p>
-              <div className="flex ">
-                <p className="txt">Status:</p>
-                <div className="flex">
-                  <div className="btn-done"> {ride.status} </div>
+        {rides.length > 0 ? (
+          rides.map((ride, index) =>
+            ride.status === "Done" ? (
+              <div className="ride-history-details flex space-between" key={index}>
+                <div className="ride-history-details-left flex column space-around">
+                  <p className="txt">FROM: {ride.location_start}</p>
+                  <p className="txt">To: {ride.location_end}</p>
+                  <p className="txt">Driver Name: {ride.driver_id}</p>
+                </div>
+                <div className="ride-history-details-right flex column space-around">
+                  <p className="txt">Request Date: {ride.created_at}</p>
+                  <div className="flex">
+                    <p className="txt">Status:</p>
+                    <div className="flex">
+                      <div className="btn-done">{ride.status}</div>
+                    </div>
+                  </div>
+                  <p className="txt">Rate: {ride.rate_user}</p>
                 </div>
               </div>
-
-              <p className="txt">Rate: {ride.rate_user}</p>
-            </div>
-          </div>
-        ))}
+            ) : null
+          )
+        ) : (
+          <p>No history</p>
+        )}
       </div>
     </div>
   );
